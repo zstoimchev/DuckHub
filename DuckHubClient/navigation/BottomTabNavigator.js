6ef1b5313b3006df26423a6d80a328b283d42c72
FiSpro <filipspro@gmail.com>
2025-04-12 23:02:32 +0200

    Added logo

--- DuckHubClient/navigation/BottomTabNavigator.js
+++ DuckHubClient/navigation/BottomTabNavigator.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -11,11 +10,10 @@ const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            let iconName;
-
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
 
           switch (route.name) {
             case "Home":
@@ -29,21 +27,21 @@ export default function BottomTabNavigator() {
               break;
           }
 
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          headerShown: false,
-          tabBarActiveTintColor: '#D98324',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: {
-            backgroundColor: '#000000',  // Set the tab bar background to black
-            borderTopWidth: 0,        // Remove the top border line (optional)
-          },
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: "#D98324",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "#000000", // Set the tab bar background to black
+          borderTopWidth: 0, // Remove the top border line (optional)
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
 
@@ -52,4 +50,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
