2b21afe23ada50136c0213fa69edd2edea521244
Zhivko <zivkostoimcev1551@outlook.com>
2025-04-12 17:26:09 +0200

    [ADD] entities created

---
+++ DuckHubApi/src/main/java/duck/dragonhack/duckhubapi/models/Following.java
@@ -0,0 +1,27 @@
+package duck.dragonhack.duckhubapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "followings")
+public class Following {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
+}
